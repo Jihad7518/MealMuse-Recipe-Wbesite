d4eefa1e591cd4518b52e8c0dd0e5d49bb261111
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-04 02:19:00 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const fileUpload = require('express-fileupload');
@@ -21,3 +20,23 @@ app.use(session({
   saveUninitialized: true,
   resave: true
 }));
+app.use(flash());
+app.use(fileUpload());
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+const routes = require('./server/routes/recipeRoutes.js')
+app.use('/', routes);
+
+// In your Express app, create a route for the Contact Us page
+app.get('/contact', (req, res) => {
+  res.render('contact'); // This will render contact.ejs
+});
+
+app.get('/about', (req, res) => {
+  res.render('about'); // This will render contact.ejs
+});
+
+
+app.listen(port, ()=> console.log(`Server is Running at Port no: ${port}`));
