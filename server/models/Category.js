8ac9628fc4fa4cf9b6d74f08bebe8efb8816b32e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-16 01:30:16 +0600

    Update Category.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,1 +1,15 @@
 
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Name is Required'
+  },
+  image: {
+    type: String,
+    required: 'Image is Required'
+  },
+});
+
+module.exports = mongoose.model('Category', categorySchema);
