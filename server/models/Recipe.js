33249501acd7c4c49b7e7c43cccc25a41d8d8f05
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-16 01:31:55 +0600

    Update Recipe.js

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,1 +1,70 @@
 
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  description: {
+    type: String,
+    required: 'This field is required.'
+  },
+  email: {
+    type: String,
+    required: 'This field is required.'
+  },
+  ingredients: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  category: {
+    type: String,
+    enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Bengali', 'Indian', 'Spanish'],
+    required: 'This field is required.'
+  },
+  image: {
+    type: String,
+    required: 'This field is required.'
+  },
+  kcal: {
+    type: Number,
+    required: true
+  },
+  fat: {
+    type: Number,
+    required: true
+  },
+  saturates: {
+    type: Number,
+    required: true
+  },
+  carbs: {
+    type: Number,
+    required: true
+  },
+  sugar: {
+    type: Number,
+    required: true
+  },
+  fiber: {
+    type: Number,
+    required: true
+  },
+  protein: {
+    type: Number,
+    required: true
+  },
+  salt: {
+    type: Number,
+    required: true
+  }
+});
+
+
+
+recipeSchema.index({ name: 'text', description: 'text' });
+// WildCard Indexing
+//recipeSchema.index({ "$**" : 'text' });
+
+module.exports = mongoose.model('Recipe', recipeSchema);
